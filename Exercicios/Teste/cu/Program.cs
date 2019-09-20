0622cc124fe494b589c378f29e6bafa7697145fe
Sotto <54954837+Sottooo@users.noreply.github.com>
2019-09-20 17:18:42 -0300

    Add files via upload

---
+++ Exercicios/Teste/cu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string cu;
+            do{               
+            System.Console.WriteLine("Você quer me dar o cu? sim ou não");
+            cu = string.Equals(Console.ReadLine());
+            }while(não");
+        }
+    }
+}
